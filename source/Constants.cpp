39eb2c0b6ddcece9094ede7a20351ac7067f5759
Nicolas Barone <njb227@cornell.edu>
2021-03-22 00:01:57 -0400

    update constants

--- source/Constants.cpp
+++ source/Constants.cpp
@@ -1,5 +1,5 @@
 #include "Constants.h"
-const float FLOOR_HEIGHT = 200;
+const float FLOOR_HEIGHT = 300;
 const float FLOOR_OFFSET = 100;
 const float DEFAULT_VISION = 300;
 const float POSSESS_RANGE = 150;
