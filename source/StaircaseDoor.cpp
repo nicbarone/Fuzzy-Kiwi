31afd8ad920b62fc28b044f3efe0bf0cab372ca3
Nicolas Barone <njb227@cornell.edu>
2021-04-02 16:24:04 -0400

    Door animations work
    used schedule method

--- source/StaircaseDoor.cpp
+++ source/StaircaseDoor.cpp
@@ -29,20 +29,60 @@ bool StaircaseDoor::init(int x, float ang, Vec2 scale, int level, Color4 color, 
 
 void StaircaseDoor::setDoor(bool open) {
 	_isOpen = open;
-	CULog("staircase door state %d", _isOpen);
-
+	std::function<bool()> frame0 = [&]() {
+		std::dynamic_pointer_cast<scene2::AnimationNode>(getSceneNode())->setFrame(0);
+		return false;
+	};
+	std::function<bool()> frame1 = [&]() {
+		std::dynamic_pointer_cast<scene2::AnimationNode>(getSceneNode())->setFrame(1);
+		return false;
+	};
+	std::function<bool()> frame2 = [&]() {
+		std::dynamic_pointer_cast<scene2::AnimationNode>(getSceneNode())->setFrame(2);
+		return false;
+	};
+	std::function<bool()> frame3 = [&]() {
+		std::dynamic_pointer_cast<scene2::AnimationNode>(getSceneNode())->setFrame(3);
+		return false;
+	};
+	std::function<bool()> frame4 = [&]() {
+		std::dynamic_pointer_cast<scene2::AnimationNode>(getSceneNode())->setFrame(4);
+		return false;
+	};
+	std::function<bool()> frame5 = [&]() {
+		std::dynamic_pointer_cast<scene2::AnimationNode>(getSceneNode())->setFrame(5);
+		return false;
+	};
+	std::function<bool()> frame6 = [&]() {
+		std::dynamic_pointer_cast<scene2::AnimationNode>(getSceneNode())->setFrame(6);
+		return false;
+	};
+	std::function<bool()> frame7 = [&]() {
+		std::dynamic_pointer_cast<scene2::AnimationNode>(getSceneNode())->setFrame(7);
+		return false;
+	};
+	std::function<bool()> frame8 = [&]() {
+		std::dynamic_pointer_cast<scene2::AnimationNode>(getSceneNode())->setFrame(8);
+		return false;
+	};
 	if (open) {
-
-		for (int i = 0; i < 7; i++) {
-			std::dynamic_pointer_cast<scene2::AnimationNode>(getSceneNode())->setFrame(i);
-		}
-		/*cugl::Application::schedule(Door::doorOpenAnimation, 3);*/
+		int timeDiff = 25;
+		cugl::Application::get()->schedule(frame1, 50 + timeDiff);
+		cugl::Application::get()->schedule(frame2, 50 + timeDiff * 2);
+		cugl::Application::get()->schedule(frame3, 50 + timeDiff * 3);
+		cugl::Application::get()->schedule(frame4, 50 + timeDiff * 4);
+		cugl::Application::get()->schedule(frame5, 50 + timeDiff * 5);
+		cugl::Application::get()->schedule(frame6, 50 + timeDiff * 6);
 	}
 	else
 	{
-		for (int i = 7; i > -1; i--) {
-			std::dynamic_pointer_cast<scene2::AnimationNode>(getSceneNode())->setFrame(i);
-		}
+		int timeDiff = 25;
+		cugl::Application::get()->schedule(frame5, 50 + timeDiff);
+		cugl::Application::get()->schedule(frame4, 50 + timeDiff * 2);
+		cugl::Application::get()->schedule(frame3, 50 + timeDiff * 3);
+		cugl::Application::get()->schedule(frame2, 50 + timeDiff * 4);
+		cugl::Application::get()->schedule(frame1, 50 + timeDiff * 5);
+		cugl::Application::get()->schedule(frame0, 50 + timeDiff * 6);
 	}
 
 }
