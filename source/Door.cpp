d341c3563d7c128cf7e6d132611b8393986a9161
Nicolas Barone <njb227@cornell.edu>
2021-04-14 16:16:14 -0400

    restore door code

--- source/Door.cpp
+++ source/Door.cpp
@@ -1,7 +1,7 @@
 #include "Door.h"
 using namespace cugl;
 
-#define DOOR_OFFSET 66
+#define DOOR_OFFSET 64
 Door::Door() :
 	_frame(0),
 	_frameCounter(8)
@@ -37,32 +37,62 @@ bool Door::init(int x, float ang, Vec2 scale, int level, Color4 color, std::vect
 
 void Door::setDoor(bool open) {
 	_isOpen = open;
-	std::function<bool()> frame0 = [this]() {
-		std::dynamic_pointer_cast<scene2::AnimationNode>(getSceneNode())->setFrame(_frameCounter);
-		_frameCounter++;
-		//old version
+	std::function<bool()> frame0 = [&]() {
+		std::dynamic_pointer_cast<scene2::AnimationNode>(getSceneNode())->setFrame(0);
 		return false;
 	};
-	std::function<bool()> frame1 = [this](){
-		std::dynamic_pointer_cast<scene2::AnimationNode>(getSceneNode())->setFrame(_frameCounter);
-		_frameCounter--;
+	std::function<bool()> frame1 = [&](){
+		std::dynamic_pointer_cast<scene2::AnimationNode>(getSceneNode())->setFrame(1);
+		return false;
+	};
+	std::function<bool()> frame2 = [&]() {
+		std::dynamic_pointer_cast<scene2::AnimationNode>(getSceneNode())->setFrame(2);
+		return false;
+	};
+	std::function<bool()> frame3 = [&]() {
+		std::dynamic_pointer_cast<scene2::AnimationNode>(getSceneNode())->setFrame(3);
+		return false;
+	};
+	std::function<bool()> frame4 = [&]() {
+		std::dynamic_pointer_cast<scene2::AnimationNode>(getSceneNode())->setFrame(4);
+		return false;
+	};
+	std::function<bool()> frame5 = [&]() {
+		std::dynamic_pointer_cast<scene2::AnimationNode>(getSceneNode())->setFrame(5);
+		return false;
+	};
+	std::function<bool()> frame6 = [&]() {
+		std::dynamic_pointer_cast<scene2::AnimationNode>(getSceneNode())->setFrame(6);
+		return false;
+	};
+	std::function<bool()> frame7 = [&]() {
+		std::dynamic_pointer_cast<scene2::AnimationNode>(getSceneNode())->setFrame(7);
+		return false;
+	};
+	std::function<bool()> frame8 = [&]() {
+		std::dynamic_pointer_cast<scene2::AnimationNode>(getSceneNode())->setFrame(8);
 		return false;
 	};
 	if (open) {
 		int timeDiff = 25;
-		_frameCounter = 1;
-		for (int i = 1; i <= 6; i = i++) {
-			cugl::Application::get()->schedule(frame0, 50 + timeDiff * i);
-		}
+		cugl::Application::get()->schedule(frame1, 50 + timeDiff);
+		cugl::Application::get()->schedule(frame2, 50 + timeDiff*2);
+		cugl::Application::get()->schedule(frame3, 50 + timeDiff*3);
+		cugl::Application::get()->schedule(frame4, 50 + timeDiff*4);
+		cugl::Application::get()->schedule(frame5, 50 + timeDiff*5);
+		cugl::Application::get()->schedule(frame6, 50 + timeDiff*6);
+		cugl::Application::get()->schedule(frame7, 50 + timeDiff*7);
 	}
 	else
 	{
 		int timeDiff = 25;
-		_frameCounter = 6;
-		for (int i = 6; i >= 0; i = i--) {
-			cugl::Application::get()->schedule(frame1, 50 + timeDiff * i);
-			CULog("frame counter %d", _frameCounter);
-		}
+		cugl::Application::get()->schedule(frame6, 50 + timeDiff);
+		cugl::Application::get()->schedule(frame5, 50 + timeDiff * 2);
+		cugl::Application::get()->schedule(frame4, 50 + timeDiff * 3);
+		cugl::Application::get()->schedule(frame3, 50 + timeDiff * 4);
+		cugl::Application::get()->schedule(frame2, 50 + timeDiff * 5);
+		cugl::Application::get()->schedule(frame1, 50 + timeDiff * 6);
+		cugl::Application::get()->schedule(frame0, 50 + timeDiff * 7);
 	}
 	//std::dynamic_pointer_cast<scene2::AnimationNode>(getSceneNode())->setFrame(_frame);
 	if (_blockedEnemy != nullptr) {
